e6f0518a57bd2dc9366e6d289bb2fd2933f12867
samkrall <95418265+samkrall@users.noreply.github.com>
2024-12-12 11:54:48 -0600

    Create Notes.py

---
+++ Notes.py
@@ -0,0 +1,12 @@
+'''
+TODO
+- Remove outliers from data for Heatmap
+- Remove duplicates from data for heatmap
+- Look for highest projected growh for zip codes in the heatmap
+- Look at highest projected growth for zip codes in aggregate
+- Drop duplicates for zip code rent/zhvi data
+- Remove outliers for zip code rent/zhvi data
+- Pull top 5th of rental ratio and top 5th of projected growth
+- Pull all data that is top 5th for both rental ratio/projected growth
+- These are the zip codes to focus on.
+'''
